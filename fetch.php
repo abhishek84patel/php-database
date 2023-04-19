514907576fe0d3d36e8b5f2924777c5f4e4ffb5b
Abhishek Patel <108073387+abhishek84patel@users.noreply.github.com>
2023-04-19 15:23:58 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,1 +1,30 @@
 
+<?php
+
+
+require_once('config/database.php');
+ $query="SELECT * FROM `student`;
+$result=mysqli_query($con,$query);
+
+
+
+
+?>
+
+
+    <?php 
+    while($row=mysqli_fetch_assoc())
+    {
+
+        ?>
+ <h1> <?php echo $row['ID'] ?></h1> 
+    
+    <?php
+    
+          }
+    ?>
+    
+    
+    
+
+  
